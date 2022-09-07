709215cf1210233cd6159207db01ddc07d6fd611
hub <code@hub.lol>
2022-09-07 22:33:24 +0200

    forge info message

--- server/forge.go
+++ server/forge.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"errors"
+	"fmt"
 )
 
 type Forge struct {
@@ -10,5 +11,7 @@ type Forge struct {
 }
 
 func (provider *Forge) Provide(serverDir string, serverFile string) error {
-	return errors.New("forge provider not yet implemented")
+	u := "https://files.minecraftforge.net/net/minecraftforge/forge/index_" + provider.MinecraftVersion + ".html"
+	fmt.Println("Please acquire the required forge server file ("+provider.ForgeVersion+") manually to continue:", u)
+	return errors.New("forge provider not implemented")
 }
