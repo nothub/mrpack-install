aafd5625b594ae47033455e6275105f5ff96e6fb
Florian Hübner <code@hub.lol>
2023-02-11 15:39:14 +0100

    backup dir format

--- update/backup/backup.go
+++ update/backup/backup.go
@@ -19,7 +19,7 @@ func SetDir(s string) {
 
 func Create(filePath string, serverDir string) error {
 	if dir == "" {
-		dir = path.Join(serverDir, "backups", time.Now().Format("2006-01-02_15-04-05"))
+		dir = path.Join(serverDir, "backups", time.Now().Format("2006-01-02-15-04-05"))
 	}
 
 	// create backup dirs
