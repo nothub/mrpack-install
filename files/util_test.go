0e14cc8564c60f39863b67b1b03e7025c3d67d89
Florian Hübner <fhuebner@posteo.de>
2025-06-14 13:58:20 +0200

    file and symlink path res test

--- files/util_test.go
+++ files/util_test.go
@@ -1,7 +1,11 @@
 package files
 
 import (
+	"fmt"
+	"os"
+	"path/filepath"
 	"testing"
+	_ "unsafe"
 )
 
 func testPathIsSubpath(t *testing.T, path string, basePath string) bool {
@@ -27,6 +31,36 @@ func TestPathTraversalRelative(t *testing.T) {
 		t.FailNow()
 	}
 	if !testPathIsSubpath(t, "../../../../../../../tmp/file", "/tmp/") {
+		t.FailNow()
+	}
+}
+
+func TestResolveSymlink(t *testing.T) {
+
+	file, err := os.CreateTemp("", "test-*")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.Remove(file.Name())
+
+	link := filepath.Join(filepath.Dir(file.Name()), fmt.Sprintf("test-%s", RandString(10)))
+	err = os.Symlink(file.Name(), link)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.Remove(link)
+
+	// resolve file
+	resolved, err := Resolve(file.Name())
+	if resolved != file.Name() {
+		t.Logf("resolved: %q but should be: %q\n", resolved, file.Name())
+		t.FailNow()
+	}
+
+	// resolve symlink
+	resolved, err = Resolve(link)
+	if resolved == link {
+		t.Logf("resolved: %q but should be: %q\n", resolved, file.Name())
 		t.FailNow()
 	}
 }
